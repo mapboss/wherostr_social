ceab226cdd4682f153617d970276e0808d3f2056
Nicky Dev <nickydev@mapboss.co.th>
2024-09-27 14:23:17 +0700

    fix: resolve reverse geocoding issue for other platforms

--- lib/services/map.dart
+++ lib/services/map.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 import 'package:maplibre_gl/maplibre_gl.dart';
@@ -36,30 +37,42 @@ class MapService {
   }
 
   static Future<Placemark> reverse(LatLng ll) async {
-    var placemarks = await placemarkFromCoordinates(ll.latitude, ll.longitude);
-    var placemark = placemarks.first;
-    if (placemark.country!.isEmpty && placemark.name!.isEmpty) {
-      placemark = Placemark(
-          name:
-              '${ll.latitude.toStringAsFixed(5)}, ${ll.longitude.toStringAsFixed(5)}');
+    if (Platform.isIOS || Platform.isAndroid) {
+      var placemarks =
+          await placemarkFromCoordinates(ll.latitude, ll.longitude);
+      var placemark = placemarks.first;
+      if (placemark.country!.isEmpty && placemark.name!.isEmpty) {
+        placemark = Placemark(
+            name:
+                '${ll.latitude.toStringAsFixed(5)}, ${ll.longitude.toStringAsFixed(5)}');
+      }
+      return placemark;
+    } else {
+      final request = http.Request(
+          'GET',
+          Uri.parse(reverseGeocodeServiceUrl).replace(queryParameters: {
+            'accept-language': 'en',
+            'format': 'jsonv2',
+            'lat': ll.latitude.toString(),
+            'lon': ll.longitude.toString()
+          }));
+
+      final response = await http.Response.fromStream(await request.send());
+      if (response.statusCode == 200) {
+        final jsonResponse = jsonDecode(response.body);
+        final result = MapReverseGeocodeResult.fromJson(jsonResponse);
+        return Placemark(
+          country: result.address?.country,
+          isoCountryCode: result.address?.countryCode,
+          administrativeArea: result.address?.state ??
+              result.address?.province ??
+              result.address?.city,
+          subAdministrativeArea: result.address?.suburb ?? result.address?.town,
+          postalCode: result.address?.postcode,
+        );
+      } else {
+        throw Exception('Failed to request reverse');
+      }
     }
-    return placemark;
-
-    // final request = http.Request(
-    //     'GET',
-    //     Uri.parse(reverseGeocodeServiceUrl).replace(queryParameters: {
-    //       'accept-language': 'en',
-    //       'format': 'jsonv2',
-    //       'lat': ll.latitude.toString(),
-    //       'lon': ll.longitude.toString()
-    //     }));
-
-    // final response = await http.Response.fromStream(await request.send());
-    // if (response.statusCode == 200) {
-    //   final jsonResponse = jsonDecode(response.body);
-    //   return MapReverseGeocodeResult.fromJson(jsonResponse);
-    // } else {
-    //   throw Exception('Failed to request reverse');
-    // }
   }
 }
