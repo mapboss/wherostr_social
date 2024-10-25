b3e1c004719841e9308b7f9c21041c8785afdb51
Nicky Dev <nickydev@mapboss.co.th>
2024-10-26 01:17:13 +0700

    add sqlite

--- lib/models/encrypted_message.dart
+++ lib/models/encrypted_message.dart
@@ -20,19 +20,21 @@ class EncryptedMessage {
   });
 
   static Future<void> init() async {
-    final dbpath = join(await getDatabasesPath(), 'encrypted_message.db');
-    await deleteDatabase(dbpath);
+    var dbpath = join(await getDatabasesPath(), 'encrypted_message.db');
     EncryptedMessage.database = await openDatabase(
       dbpath,
       onCreate: (db, version) async {
         print('onCreate');
-        await db.execute('''BEGIN;
-CREATE TABLE encrypted_message(id TEXT PRIMARY KEY, encryptedContent TEXT, sender TEXT, reciever TEXT, createdAt INTEGER, sig TEXT);
-CREATE UNIQUE INDEX encrypted_message_id_idx ON encrypted_message (id);
-CREATE INDEX encrypted_message_sender_idx ON encrypted_message (sender);
-CREATE INDEX encrypted_message_reciever_idx ON encrypted_message (reciever);
-CREATE INDEX encrypted_message_sender_reciever_idx ON encrypted_message (sender,reciever);
-COMMIT;''');
+        await db.execute(
+            'CREATE TABLE encrypted_message(id TEXT PRIMARY KEY, encryptedContent TEXT, sender TEXT, reciever TEXT, createdAt INTEGER, sig TEXT)');
+        await db.execute(
+            'CREATE UNIQUE INDEX encrypted_message_id_idx ON encrypted_message (id);');
+        await db.execute(
+            'CREATE INDEX encrypted_message_sender_idx ON encrypted_message (sender);');
+        await db.execute(
+            'CREATE INDEX encrypted_message_reciever_idx ON encrypted_message (reciever);');
+        await db.execute(
+            'CREATE INDEX encrypted_message_sender_reciever_idx ON encrypted_message (sender,reciever);');
       },
       version: 1,
     );
--- lib/widgets/messages_container.dart
+++ lib/widgets/messages_container.dart
@@ -21,19 +21,19 @@ import 'package:wherostr_social/widgets/post_content.dart';
 class MessagesContainer extends StatelessWidget {
   const MessagesContainer({super.key});
 
-  Future<List<DataEvent>> initialize(BuildContext context) async {
-    final appState = context.read<AppStatesProvider>();
-    return NostrService.instance.fetchEvents(
-      [
-        NostrFilter(
-          kinds: [4, 1059],
-          p: [appState.me.pubkey],
-        )
-      ],
-      eoseRatio: 1,
-      timeout: Duration(seconds: 120),
-    );
-  }
+  // Future<List<DataEvent>> initialize(BuildContext context) async {
+  //   final appState = context.read<AppStatesProvider>();
+  //   return NostrService.instance.fetchEvents(
+  //     [
+  //       NostrFilter(
+  //         kinds: [4, 1059],
+  //         p: [appState.me.pubkey],
+  //       )
+  //     ],
+  //     eoseRatio: 1,
+  //     timeout: Duration(seconds: 120),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -41,132 +41,133 @@ class MessagesContainer extends StatelessWidget {
     ThemeData themeData = Theme.of(context);
     final appState = context.read<AppStatesProvider>();
     return FutureBuilder(
-      future: initialize(context),
-      builder: (context, AsyncSnapshot<List<DataEvent>> snapshot) {
+      // future: initialize(context),
+      future: Future.wait([]),
+      builder: (context, snapshot) {
         return Scaffold(
           appBar: AppBar(
             title: const Text('Messages'),
           ),
-          body: snapshot.connectionState != ConnectionState.done
-              ? CircularProgressIndicator()
-              : Text(snapshot.data?.length.toString() ?? ''),
-          // body: NostrFeed(
-          //   backgroundColor: themeData.colorScheme.primary.withOpacity(0.054),
-          //   scrollController: ScrollController(),
-          //   relays: appState.me.relayList.clone(),
-          //   kinds: const [4, 1059],
-          //   p: [appState.me.pubkey],
-          //   disableLimit: true,
-          //   itemMapper: (e) async {
-          //     final keyPairs = await AppSecret.read();
-          //     if (e.kind == 1059) {
-          //       final json = await Nip44.decrypt(
-          //           e.content!, Nip44.shareSecret(keyPairs!.private, e.pubkey));
-          //       final newEvent = DataEvent.fromJson(jsonDecode(json));
-          //       newEvent.addTagIfNew(['p', e.pubkey]);
-          //       return newEvent;
-          //     }
-          //     return e;
-          //   },
-          //   itemFilter: (e) {
-          //     if (e.kind != 4 && e.kind != 13) return false;
-          //     if (events.containsKey(e.pubkey)) return false;
-          //     if (e.pubkey == appState.me.pubkey) return false;
-          //     events[e.pubkey] = true;
-          //     return true;
-          //   },
-          //   itemBuilder: (context, event) {
-          //     final content = getEllipsisText(
-          //       text: event.content!,
-          //       maxWidth: (MediaQuery.sizeOf(context).width >=
-          //                   Constants.largeDisplayWidth
-          //               ? Constants.largeDisplayContentWidth
-          //               : MediaQuery.sizeOf(context).width) -
-          //           76,
-          //       maxLines: 3,
-          //     );
-          //     return Material(
-          //       child: Column(
-          //         children: [
-          //           InkWell(
-          //             onTap: () {
-          //               final appState = context.read<AppStatesProvider>();
-          //               appState.navigatorPush(
-          //                 widget: Scaffold(
-          //                   appBar: AppBar(
-          //                     title: const Text('Chat'),
-          //                   ),
-          //                   body: NostrFeed(
-          //                     backgroundColor: themeData.colorScheme.primary
-          //                         .withOpacity(0.054),
-          //                     scrollController: ScrollController(),
-          //                     relays: appState.me.relayList.clone(),
-          //                     kinds: const [4, 1059],
-          //                     authors: [event.pubkey, appState.me.pubkey],
-          //                     p: [appState.me.pubkey],
-          //                     autoRefresh: true,
-          //                     reverse: true,
-          //                     isDynamicHeight: true,
-          //                     itemMapper: (e) async {
-          //                       final keyPairs = await AppSecret.read();
-          //                       if (e.kind == 1059) {
-          //                         return Nip17.decode(e, keyPairs!.private);
-          //                       }
-          //                       final msg = await Nip4.decode(
-          //                           e, keyPairs!.public, keyPairs.private);
-          //                       e.content = msg?.content;
-          //                       return e;
-          //                     },
-          //                     itemFilter: (e) {
-          //                       if (e.kind != 4 && e.kind != 14) return false;
-          //                       if (e.pubkey != event.pubkey &&
-          //                           e.pubkey != appState.me.pubkey) {
-          //                         return false;
-          //                       }
-          //                       if (e.pubkey == appState.me.pubkey &&
-          //                           e.getTagValue('p') != event.pubkey) {
-          //                         print(
-          //                             '${e.getTagValue('p')} | ${event.pubkey}');
-          //                         return false;
-          //                       }
-          //                       return true;
-          //                     },
-          //                     itemBuilder: (context, event) => Padding(
-          //                       padding:
-          //                           const EdgeInsets.fromLTRB(16, 0, 16, 12),
-          //                       child: MessageItem(
-          //                         event: event,
-          //                         isCompact: false,
-          //                         enableActionBar: true,
-          //                       ),
-          //                     ),
-          //                   ),
-          //                 ),
-          //               );
-          //             },
-          //             child: Padding(
-          //               padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
-          //               child: Column(
-          //                 crossAxisAlignment: CrossAxisAlignment.start,
-          //                 children: [
-          //                   PostComposer(event: event, enableMenu: false),
-          //                   PostContent(
-          //                     content: content.trim(),
-          //                     enableMedia: false,
-          //                     enablePreview: false,
-          //                     enableElementTap: false,
-          //                     depth: 1,
-          //                   ),
-          //                 ],
-          //               ),
-          //             ),
-          //           ),
-          //           const Divider(height: 1),
-          //         ],
-          //       ),
-          //     );
-          //   },
-          // ),
+          // body: snapshot.connectionState != ConnectionState.done
+          //     ? CircularProgressIndicator()
+          //     : Text(snapshot.data?.length.toString() ?? ''),
+          body: NostrFeed(
+            backgroundColor: themeData.colorScheme.primary.withOpacity(0.054),
+            scrollController: ScrollController(),
+            relays: appState.me.relayList.clone(),
+            kinds: const [4, 1059],
+            p: [appState.me.pubkey],
+            disableLimit: true,
+            itemMapper: (e) async {
+              final keyPairs = await AppSecret.read();
+              if (e.kind == 1059) {
+                final json = await Nip44.decrypt(
+                    e.content!, Nip44.shareSecret(keyPairs!.private, e.pubkey));
+                final newEvent = DataEvent.fromJson(jsonDecode(json));
+                newEvent.addTagIfNew(['p', e.pubkey]);
+                return newEvent;
+              }
+              return e;
+            },
+            itemFilter: (e) {
+              if (e.kind != 4 && e.kind != 13) return false;
+              if (events.containsKey(e.pubkey)) return false;
+              if (e.pubkey == appState.me.pubkey) return false;
+              events[e.pubkey] = true;
+              return true;
+            },
+            itemBuilder: (context, event) {
+              final content = getEllipsisText(
+                text: event.content!,
+                maxWidth: (MediaQuery.sizeOf(context).width >=
+                            Constants.largeDisplayWidth
+                        ? Constants.largeDisplayContentWidth
+                        : MediaQuery.sizeOf(context).width) -
+                    76,
+                maxLines: 3,
+              );
+              return Material(
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        final appState = context.read<AppStatesProvider>();
+                        appState.navigatorPush(
+                          widget: Scaffold(
+                            appBar: AppBar(
+                              title: const Text('Chat'),
+                            ),
+                            body: NostrFeed(
+                              backgroundColor: themeData.colorScheme.primary
+                                  .withOpacity(0.054),
+                              scrollController: ScrollController(),
+                              relays: appState.me.relayList.clone(),
+                              kinds: const [4, 1059],
+                              authors: [event.pubkey, appState.me.pubkey],
+                              p: [appState.me.pubkey],
+                              autoRefresh: true,
+                              reverse: true,
+                              isDynamicHeight: true,
+                              itemMapper: (e) async {
+                                final keyPairs = await AppSecret.read();
+                                if (e.kind == 1059) {
+                                  return Nip17.decode(e, keyPairs!.private);
+                                }
+                                final msg = await Nip4.decode(
+                                    e, keyPairs!.public, keyPairs.private);
+                                e.content = msg?.content;
+                                return e;
+                              },
+                              itemFilter: (e) {
+                                if (e.kind != 4 && e.kind != 14) return false;
+                                if (e.pubkey != event.pubkey &&
+                                    e.pubkey != appState.me.pubkey) {
+                                  return false;
+                                }
+                                if (e.pubkey == appState.me.pubkey &&
+                                    e.getTagValue('p') != event.pubkey) {
+                                  print(
+                                      '${e.getTagValue('p')} | ${event.pubkey}');
+                                  return false;
+                                }
+                                return true;
+                              },
+                              itemBuilder: (context, event) => Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(16, 0, 16, 12),
+                                child: MessageItem(
+                                  event: event,
+                                  isCompact: false,
+                                  enableActionBar: true,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            PostComposer(event: event, enableMenu: false),
+                            PostContent(
+                              content: content.trim(),
+                              enableMedia: false,
+                              enablePreview: false,
+                              enableElementTap: false,
+                              depth: 1,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const Divider(height: 1),
+                  ],
+                ),
+              );
+            },
+          ),
         );
       },
     );
