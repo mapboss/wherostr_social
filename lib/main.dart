c65d3ce3f541b5b61181c8fa4b08cc6e7bd2cf43
Nicky Dev <nickydev@mapboss.co.th>
2024-11-05 12:56:06 +0700

    fix: resolve error by deleting missing app_message.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:provider/provider.dart';
 import 'package:wherostr_social/constant.dart';
 import 'package:wherostr_social/models/app_feed.dart';
 import 'package:wherostr_social/models/app_locale.dart';
-import 'package:wherostr_social/models/app_messaging.dart';
 import 'package:wherostr_social/models/app_notification.dart';
 import 'package:wherostr_social/models/app_relays.dart';
 import 'package:wherostr_social/models/app_settings.dart';
@@ -84,7 +83,6 @@ class MainApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => AppThemeProvider()),
         ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
         ChangeNotifierProvider(create: (_) => AppNotificationProvider()),
-        ChangeNotifierProvider(create: (_) => AppMessagingProvider()),
         ChangeNotifierProvider(create: (_) => AppStatesProvider()),
       ],
       child: Consumer2<AppLocaleProvider, AppThemeProvider>(
